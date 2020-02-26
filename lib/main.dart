3a573a91a8a4fb1e2121368ce01478800ead4e0d
Wayne <waynevick7226@hotmail.com>
2020-02-26 14:01:42 -0500

    added transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +15,22 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions =[
+    Transaction(
+      id: 't1',
+      title: 'new shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+
+    Transaction(
+      id: 't2',
+      title: 'new clothes',
+      amount: 29.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
